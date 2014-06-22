b8f2cf50ccd35a601a8568caf72298962bbc6474
Aniket Kudale <aniket.kudale@hotmail.com>
2014-06-22 16:45:43 +0530

    Update control.js

--- control.js
+++ control.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
 	function run()
 	{
 		
-		d = "right"; //default direction of saap
+	  d = "right"; //default direction of saap
 	  create_snake();
 	  create_food();
 	  
